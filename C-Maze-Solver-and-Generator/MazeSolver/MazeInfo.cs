37059d4061e72fd6a759be8f5831c195ea6fa7c8
shawn-higgins1 <23224097+shawn-higgins1@users.noreply.github.com>
2018-05-20 15:46:57 -0400

    Create Maze solver/generator

---
+++ C-Maze-Solver-and-Generator/MazeSolver/MazeInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeSolver
+{
+    public class MazeInfo
+    {
+        public string MazeWidth { get; set; }
+        public string MazeHeight { get; set; }
+    }
+}
